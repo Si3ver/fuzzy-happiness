f16151e34bb7fa5aa48a2de7ae94297cfec2533e
si3ver <izhouwl@163.com>
2021-09-19 02:47:08 +0800

    feat: esm + amd + require.js

--- ts_namespace/src/page.ts
+++ ts_namespace/src/page.ts
@@ -1,10 +1,8 @@
-/// <reference path='./components.ts' />
-
-namespace Home {  export class Page {
-    constructor () {
-      new Components.Header()
-      new Components.Content()
-      new Components.Footer()
-    }
+import { Header, Content, Footer } from "./components";
+export default class Page {
+  constructor() {
+    new Header();
+    new Content();
+    new Footer();
   }
 }
