f3f5b785c0555eb11051d8226d83dad5753b8c61
si3ver <izhouwl@163.com>
2021-09-19 03:36:42 +0800

    使用interface的方式实现函数重载

--- ts_parcel/src/jquery.d.ts
+++ ts_parcel/src/jquery.d.ts
@@ -3,8 +3,18 @@
 
 // 定义全局函数
 interface JqueryInstance {
-  html: (html: string) => {}
+  html: (html: string) => JqueryInstance
 }
 
-declare function $(readyFunc: () => void): void
-declare function $(selector: string): JqueryInstance
+// 函数重载
+// declare function $(readyFunc: () => void): void
+// declare function $(selector: string): JqueryInstance
+
+// 使用interface的方式实现函数重载
+interface JQuery {
+  (readyFunc: () => void): void
+  (selector: string): JqueryInstance
+}
+
+declare var $: Jquery
+
