f8cd964e29be07fa47716c8e74dc5119a5f48329
si3ver <izhouwl@163.com>
2021-09-19 16:32:53 +0800

    增加password验证

--- ts_crawler/src/index.ts
+++ ts_crawler/src/index.ts
@@ -1,9 +1,10 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
 import router from './router'
 
 const app = express()
 const port = 7001
 
+app.use(express.urlencoded({ extended: false }))
 app.use(router)
 
 app.listen(port, () => {
--- ts_crawler/src/router.ts
+++ ts_crawler/src/router.ts
@@ -5,16 +5,29 @@ import Crawler from './crawler'
 const router = Router()
 
 router.get('/', (req, res) => {
-  res.send('hello world')
+  res.send(`
+    <html>
+      <body>
+        <form method="post" action="/getData">
+          <input type="password" name="password" />
+          <button>提交</button>
+        </form>
+      </body>
+    </html>
+  `)
 })
 
-router.get('/getData', (req, res) => {
-  const secret = 'x3b174jsx'
-  const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`
-  const analyzer = DellAnalyzer.getInstance()
-  new Crawler(url, analyzer)
-
-  res.send('getData success')
+router.post('/getData', (req, res) => {
+  console.log(req.body)
+  if (req.body.password === '123') {
+    const secret = 'x3b174jsx'
+    const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`
+    const analyzer = DellAnalyzer.getInstance()
+    new Crawler(url, analyzer)
+    res.send('getData success')
+  } else {
+    res.send('password Error!')
+  }
 })
 
 export default router
