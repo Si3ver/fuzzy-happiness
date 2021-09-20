2c1abed0db3aa9739c87cb45f72c145402ef02ac
si3ver <izhouwl@163.com>
2021-09-20 16:26:33 +0800

    fix: 类装饰器

---
+++ ts_decorator/src/simple_decorator.ts
@@ -0,0 +1,20 @@
+
+// function testDecorator(flag: boolean) {
+//   if (flag) {
+//     return function(constructor: any) {
+//       constructor.prototype.getName = () => {
+//         console.log('william')
+//       }
+//     }
+//   } else {
+//     return function() {
+
+//     }
+//   }
+// }
+
+// @testDecorator(true)
+// class Test {}
+
+// const test = new Test()
+// ;(test as any).getName()
